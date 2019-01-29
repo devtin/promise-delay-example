742d9445cd4a9ca89d9a08721a210efd13bdbbb5
Martin Rafael Gonzalez <tin@devtin.io>
2019-01-29 15:19:00 -0500

    add phrases to print

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const whatToPrint = ['eseC 1#', 'eD   ', 'nóicaprusU   ', 'onreiboG #2', 'eD   ', 'nóicisnarT   ', 'senoiccelE #3', 'serbiL   ']
+
 const delay = (ms) => new Promise(
     (resolve) => setTimeout(resolve, ms)
 )
