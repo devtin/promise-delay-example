d81a334ab35f07ae10c1cef1f708aa06b144323c
Martin Rafael Gonzalez <tin@devtin.io>
2019-01-29 15:20:40 -0500

    add myFirstAsync function printing delayed phrases

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const whatToPrint = ['eseC 1#', 'eD   ', 'nóicaprusU   ', 'onreiboG #2', 'eD   
 const delay = (ms) => new Promise(
     (resolve) => setTimeout(resolve, ms)
 )
+
+async function myFirstAsync () {
+    for (let i = 0; i < whatToPrint.length; i++) {
+        // reverse and print the line
+        console.log(whatToPrint[i].split('').reverse().join(''))
+
+        // delays the execution
+        await delay(500)
+    }
+}
